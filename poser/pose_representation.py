0426fc1a5bb2db051b94a44c343c065ad2697d4b
Stefan Blaser <stefan.blaser@inovitas.ch>
2023-08-03 07:59:49 +0200

    Extended pose representation
    Extended with:
    - Multiply magic method
    - as_tuple property

--- poser/pose_representation.py
+++ poser/pose_representation.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 from poser.rotation_handler import Rotation
 from poser.translation_representation import Translation
@@ -10,8 +11,34 @@ class Pose(object):
     rotation: Rotation
 
     def __invert__(self) -> "Pose":
-        new_rot = ~self.rotation
+        inverted_rotation = ~self.rotation
         return Pose(
-            translation=new_rot.transformed_point(~self.translation),
-            rotation=new_rot,
+            translation=inverted_rotation.transformed_point(~self.translation),
+            rotation=inverted_rotation,
         )
+    
+    def __mul__(self, other: "Pose"):
+        return Pose(
+            translation=self.translation + self.rotation.transformed_point(
+                other.translation
+            ),
+            rotation=self.rotation * other.rotation,
+        )
+    
+    @property
+    def as_tuple(self) -> Tuple[Tuple[float]]:
+        r = self.rotation.as_tuple
+        t = self.translation.as_tuple
+        return (
+            (r[0][0], r[0][1], r[0][2], t[0],),
+            (r[1][0], r[1][1], r[1][2], t[1],),
+            (r[2][0], r[2][1], r[2][2], t[2],),
+            (0.0, 0.0, 0.0, 1.0,),
+        )
+    
+    @as_tuple.setter
+    def as_tuple(self, matrix: Tuple[Tuple[float]]) -> None:
+        self.rotation.as_tuple = (
+            matrix[0][:3], matrix[1][:3], matrix[2][:3]
+        )
+        self.translation.as_tuple = (matrix[0][3], matrix[1][3], matrix[2][3])
--- tests/test_pose_representation.py
+++ tests/test_pose_representation.py
@@ -27,6 +27,65 @@ class TestPoseMagics(unittest.TestCase):
         self.assertAlmostEqual(res.translation.x, -1.0, places=4)
         self.assertAlmostEqual(res.rotation.opk_deg.omega, -10.0, places=4)
 
+    def test_multiply(self) -> None:
+        pose = poser.Pose(
+            translation=poser.Translation(x=1.0, y=2.0, z=3.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(
+                    omega=10.0, phi=20.0, kappa=30.0,
+                )
+            ),
+        )
+        res = ~pose * pose
+        self.assertIsInstance(res, poser.Pose)
+        self.assertAlmostEqual(res.translation.x, 0.0, places=4)
+        self.assertAlmostEqual(res.translation.y, 0.0, places=4)
+        self.assertAlmostEqual(res.translation.z, 0.0, places=4)
+        self.assertAlmostEqual(res.rotation.opk_deg.omega, 0.0, places=4)
+        self.assertAlmostEqual(res.rotation.opk_deg.phi, 0.0, places=4)
+        self.assertAlmostEqual(res.rotation.opk_deg.kappa, 0.0, places=4)
+
+class TestPoseProperties(unittest.TestCase):
+    def test_as_tuple_getter(self) -> None:
+        pose = poser.Pose(
+            translation=poser.Translation(), 
+            rotation=poser.Rotation(
+                rotation=poser.RotationQuaternion(),
+            )
+        )
+        matrix = pose.as_tuple
+        self.assertIsInstance(matrix, tuple)
+        self.assertTupleEqual(
+            matrix,
+            (
+                (1.0, 0.0, 0.0, 0.0), 
+                (0.0, 1.0, 0.0, 0.0), 
+                (0.0, 0.0, 1.0, 0.0), 
+                (0.0, 0.0, 0.0, 1.0),
+            )
+        )
+        
+    def test_as_tuple_setter(self) -> None:
+        pose = poser.Pose(
+            translation=poser.Translation(), 
+            rotation=poser.Rotation(
+                rotation=poser.RotationQuaternion(),
+            )
+        )
+        pose.as_tuple = (
+            (1.0, 0.0, 0.0, 1.0), 
+            (0.0, 0.0, -1.0, 2.0), 
+            (0.0, 1.0, 0.0, 3.0), 
+            (0.0, 0.0, 0.0, 1.0)
+        )
+        self.assertIsInstance(pose, poser.Pose)
+        self.assertAlmostEqual(pose.translation.x, 1.0, places=4)
+        self.assertAlmostEqual(pose.translation.y, 2.0, places=4)
+        self.assertAlmostEqual(pose.translation.z, 3.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.omega, 90.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.phi, 0.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.kappa, 0.0, places=4)
+
 
 if __name__ == "__main__":
     unittest.main()
