2b941204a247c8e1d4386bb54713479699a067b2
Stefan Blaser <blaserstefan@hotmail.com>
2023-08-03 21:36:28 +0200

    Extended pose representation
    - Added as_list property
    - Linear interpolation (interpolated)
    - transform point
    - transform points
    - transformed point
    - transformed points

--- poser/pose_representation.py
+++ poser/pose_representation.py
@@ -1,26 +1,38 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, List, Optional, Union
 
+from poser.point_representation import Point
 from poser.rotation_handler import Rotation
 from poser.translation_representation import Translation
 
 
 @dataclass
 class Pose(object):
-    translation: Translation
-    rotation: Rotation
+    translation: Optional[Translation] = None
+    rotation: Optional[Rotation] = None
+
+    def __post_init__(self) -> None:
+        if not self.translation:
+            self.translation = Translation()
+        if not self.rotation:
+            self.rotation = Rotation()
 
     def __invert__(self) -> "Pose":
         inverted_rotation = ~self.rotation
         return Pose(
-            translation=inverted_rotation.transformed_point(~self.translation),
+            translation=inverted_rotation.transformed_point(
+                ~self.translation
+            ),
             rotation=inverted_rotation,
         )
     
     def __mul__(self, other: "Pose"):
         return Pose(
-            translation=self.translation + self.rotation.transformed_point(
-                other.translation
+            translation=(
+                self.translation 
+                + self.rotation.transformed_point(
+                    other.translation
+                )
             ),
             rotation=self.rotation * other.rotation,
         )
@@ -41,4 +53,76 @@ class Pose(object):
         self.rotation.as_tuple = (
             matrix[0][:3], matrix[1][:3], matrix[2][:3]
         )
-        self.translation.as_tuple = (matrix[0][3], matrix[1][3], matrix[2][3])
+        self.translation.as_tuple = (
+            matrix[0][3], matrix[1][3], matrix[2][3]
+        )
+
+    @property
+    def as_list(self) -> List[List[float]]:
+        r = self.rotation.as_list
+        t = self.translation.as_list
+        return [
+            r[0] + [t[0]],
+            r[1] + [t[1]],
+            r[2] + [t[2]],
+            [0.0, 0.0, 0.0, 1.0],
+        ]        
+
+    @as_list.setter
+    def as_list(self, matrix: List[List[float]]) -> None:
+        self.rotation.as_list = (
+            matrix[0][:3], matrix[1][:3], matrix[2][:3]
+        )
+        self.translation.as_list = (
+            matrix[0][3], matrix[1][3], matrix[2][3]
+        )
+
+    def interpolated(
+        self,
+        other: "Pose",
+        factor: float,
+    ) -> "Pose":
+        return Pose(
+            translation=self.translation.interpolated(
+                other=other.translation, 
+                factor=factor,
+            ),
+            rotation=self.rotation.interpolated(
+                other=other.rotation,
+                factor=factor,
+            ),
+        )
+    
+    def transform_point(
+        self,
+        point: Union[Point, Translation],
+    ) -> None:
+        self.rotation.transform_point(point=point)
+        self.translation.transform_point(point=point)
+
+    def transform_points(
+        self,
+        points: Union[list, tuple],
+    ) -> None:
+        self.rotation.transform_points(points=points)
+        self.translation.transform_points(points=points)
+
+    def transformed_point(
+        self,
+        point: Union[Point, Translation],
+    ) -> Union[Point, Translation]:
+        return self.translation.transformed_point(
+            point=self.rotation.transformed_point(
+                point=point,
+            )
+        )
+    
+    def transformed_points(
+        self,
+        points: Union[list, tuple],
+    ) -> Union[list, tuple]:
+        return self.translation.transformed_points(
+            points=self.rotation.transformed_points(
+                points=points,
+            )
+        )
--- tests/test_pose_representation.py
+++ tests/test_pose_representation.py
@@ -5,10 +5,7 @@ import poser
 
 class TestPoseInit(unittest.TestCase):
     def test_init_empty(self) -> None:
-        pose = poser.Pose(
-            translation=poser.Translation(),
-            rotation=poser.Rotation(),
-        )
+        pose = poser.Pose()
         self.assertIsInstance(pose, poser.Pose)
         self.assertIsInstance(pose.rotation, poser.Rotation)
         self.assertIsInstance(pose.translation, poser.Translation)
@@ -47,12 +44,7 @@ class TestPoseMagics(unittest.TestCase):
 
 class TestPoseProperties(unittest.TestCase):
     def test_as_tuple_getter(self) -> None:
-        pose = poser.Pose(
-            translation=poser.Translation(), 
-            rotation=poser.Rotation(
-                rotation=poser.RotationQuaternion(),
-            )
-        )
+        pose = poser.Pose()
         matrix = pose.as_tuple
         self.assertIsInstance(matrix, tuple)
         self.assertTupleEqual(
@@ -85,6 +77,101 @@ class TestPoseProperties(unittest.TestCase):
         self.assertAlmostEqual(pose.rotation.opk_deg.omega, 90.0, places=4)
         self.assertAlmostEqual(pose.rotation.opk_deg.phi, 0.0, places=4)
         self.assertAlmostEqual(pose.rotation.opk_deg.kappa, 0.0, places=4)
+
+    def test_as_list_getter(self) -> None:
+        pose = poser.Pose()
+        matrix = pose.as_list
+        self.assertIsInstance(matrix, list)
+        self.assertListEqual(
+            matrix,
+            [
+                [1.0, 0.0, 0.0, 0.0], 
+                [0.0, 1.0, 0.0, 0.0], 
+                [0.0, 0.0, 1.0, 0.0], 
+                [0.0, 0.0, 0.0, 1.0],
+            ]
+        )
+        
+    def test_as_list_setter(self) -> None:
+        pose = poser.Pose()
+        pose.as_list = [
+            [1.0, 0.0, 0.0, 1.0], 
+            [0.0, 0.0, -1.0, 2.0], 
+            [0.0, 1.0, 0.0, 3.0], 
+            [0.0, 0.0, 0.0, 1.0],
+        ]
+        self.assertIsInstance(pose, poser.Pose)
+        self.assertAlmostEqual(pose.translation.x, 1.0, places=4)
+        self.assertAlmostEqual(pose.translation.y, 2.0, places=4)
+        self.assertAlmostEqual(pose.translation.z, 3.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.omega, 90.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.phi, 0.0, places=4)
+        self.assertAlmostEqual(pose.rotation.opk_deg.kappa, 0.0, places=4)
+
+
+class TestPoseMethods(unittest.TestCase):
+    def test_interpolated(self) -> None:
+        p1 = poser.Pose(
+            translation=poser.Translation(x=1.0),
+        )
+        p2 = poser.Pose(
+            translation=poser.Translation(x=2.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(omega=90.0)
+            )
+        )
+        p_i = p1.interpolated(other=p2, factor=0.5)
+        self.assertIsInstance(p_i, poser.Pose)
+        self.assertAlmostEqual(p_i.translation.x, 1.5, places=4)
+        self.assertAlmostEqual(p_i.rotation.opk_deg.omega, 45.0, places=4)
+
+    def test_transform_point(self) -> None:
+        point = poser.Point(x=0.0, y=1.414, z=1.414)
+        pose = poser.Pose(
+            translation=poser.Translation(z=1.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(omega=45.0)
+            )
+        )
+        pose.transform_point(point=point)
+        self.assertIsInstance(point, poser.Point)
+        self.assertAlmostEqual(point.z, 3.0, places=2)
+
+    def test_transform_points(self) -> None:
+        points = [poser.Point(x=0.0, y=1.414, z=1.414)]
+        pose = poser.Pose(
+            translation=poser.Translation(z=1.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(omega=45.0)
+            )
+        )
+        pose.transform_points(points=points)
+        self.assertIsInstance(points[0], poser.Point)
+        self.assertAlmostEqual(points[0].z, 3.0, places=2)
+
+    def test_transformed_point(self) -> None:
+        point = poser.Point(x=0.0, y=1.414, z=1.414)
+        pose = poser.Pose(
+            translation=poser.Translation(z=1.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(omega=45.0)
+            )
+        )
+        new_point = pose.transformed_point(point=point)
+        self.assertIsInstance(new_point, poser.Point)
+        self.assertAlmostEqual(new_point.z, 3.0, places=2)
+
+    def test_transformed_points(self) -> None:
+        points = [poser.Point(x=0.0, y=1.414, z=1.414)]
+        pose = poser.Pose(
+            translation=poser.Translation(z=1.0),
+            rotation=poser.Rotation(
+                rotation=poser.RotationOPKDeg(omega=45.0)
+            )
+        )
+        new_points = pose.transformed_points(points=points)
+        self.assertIsInstance(new_points[0], poser.Point)
+        self.assertAlmostEqual(new_points[0].z, 3.0, places=2)             
 
 
 if __name__ == "__main__":
